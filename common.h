7041f2d66118a2f962e11131d0789f141e276faf
laia guardia <laia.guardia01@estudiant.upf.edu>
2024-05-27 21:03:18 +0200

    jdhkjd

--- common.h
+++ common.h
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 #define SIZE_NAME 400
 #define SIZE_SKILLS 4
 #define SIZE_DESCRIPTION 1000
 #define TEXT 2000
 #define MAX_OPTIONS 2
+#define TOTAL_SKILLS 4
 
 #define OPTION_1 1
 #define OPTION_2 2
