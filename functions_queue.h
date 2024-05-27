c99d40094f134c6cc8a181bb23dd2e0f19d07edb
carlaferrer03 <carla.ferrer03@estudiant.upf.edu>
2024-05-27 20:47:29 +0200

    combat and carlas function

---
+++ functions_queue.h
@@ -0,0 +1,40 @@
+#include "structs.h"
+#include "common.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+//---------------------FUNCTIONS FOR THE QUEUE--------------------------
+
+// Define the structure for the queue node
+typedef struct Node {
+    const char *data;
+    struct Node *next;
+} Node;
+
+// Define the structure for the queue
+typedef struct Queue {
+    Node *front;
+    Node *rear;
+} Queue;
+
+// Function to create a new node
+Node* createNode(const char *data);
+
+// Function to initialize the queue
+void initializeQueue(Queue *q);
+
+// Function to check if the queue is empty
+int isEmpty(Queue *q);
+
+// Function to enqueue an element
+void enqueue(Queue *q, const char *data);
+
+// Function to dequeue an element
+const char* dequeue(Queue *q);
+
+// Function to print the queue
+void printQueue(Queue *q);
+
+void combat(Enemy *enemy, Character current_character);
---
+++ game_over_funct.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void game_over_funct() {
+    printf("You've lost the game :( try again some other time.");
+    exit(0); // Exit the program
+}
